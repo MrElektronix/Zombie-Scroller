ef36f239ef6b141d766fa1a77b4201f35e61afe7
MrElektronix <milovanpelt@gmail.com>
2019-05-04 01:24:07 +0200

    Player Move + extra setup
    - Player can move and change sprite depending on which way its gonna walk
    - Created CharacterBase class
    - Added player sprite
    - Added maps for scripts, prefabs and sprites

---
+++ 2DZombieGame/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : CharacterBase
+{
+    private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        Lives = 3;
+        Speed = 8f;
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public override void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _spriteRenderer.sprite = sprites[0];
+            gameObject.transform.position -= new Vector3(Speed, 0f, 0f) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _spriteRenderer.sprite = sprites[1];
+            gameObject.transform.position += new Vector3(Speed, 0f, 0f) * Time.deltaTime;
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
